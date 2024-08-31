fb56dd1a16dcbb9fa40b7c2378acb04d35a65fae
unknown <mariajosemurciamartinez85@gmail.com>
2024-08-31 00:51:22 -0500

    Se actualiza

--- ModuloSecurity/Business/obj/Debug/net8.0/Business.AssemblyInfo.cs
+++ ModuloSecurity/Business/obj/Debug/net8.0/Business.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Business")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+62b262c89e714c0e7eef8933494b04f8dba8588e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Business")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Business")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
